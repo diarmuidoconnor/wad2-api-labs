9b28674b82f22be42c4be487aa9e16cf812d1d3b
Diarmuid O Connor <doconnor@wit.ie>
2021-11-17 18:06:02 +0000

    Week 9 Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import moviesRouter from './api/movies';
 import './db';
 import './seedData'
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use('/api/movies', moviesRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
