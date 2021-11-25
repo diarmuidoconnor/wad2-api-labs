eee6fbd85be539226317fb3f836736369e877cd7
Diarmuid O Connor <doconnor@wit.ie>
2021-11-25 11:52:56 +0000

    Topic 10 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -8,6 +8,8 @@ import genresRouter from './api/genres';
 
 import './db';
 import './seedData'
+import session from 'express-session';
+import authenticate from './authenticate';
 
 const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
@@ -25,9 +27,14 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/genres', genresRouter);
 app.use(errHandler);
