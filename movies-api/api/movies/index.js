d93e886438be6e653470d324c692c33366c843bd
Diarmuid O Connor <doconnor@wit.ie>
2021-11-25 18:11:29 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,24 +1,25 @@
 import express from 'express';
 import {moviesObject} from './movies';
+import movieModel from './movieModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
-router.get('/', (req, res) => {
-  res.status(200).send(moviesObject);
-});
+router.get('/', asyncHandler(async (req, res) => {
+  const movies = await movieModel.find();
+  res.status(200).json(movies);
+}));
 
 //get a movie
-router.get('/:id', (req, res) => {
-  const key =  parseInt(req.params.id);
-  const index = moviesObject.movies.map((movie)=>{
-return movie.id;
-}).indexOf(key);
- if (index > -1) {
-     res.status(200).send(moviesObject.movies[index]);
- } else {
-   res.status(404).send({message: `Unable to find movie with id: ${key}.`, status: 404});
-   }
-});
+router.get('/:id', asyncHandler(async (req, res) => {
+  const id = parseInt(req.params.id);
+  const movie = await movieModel.findByMovieDBId(id);
+  if (movie) {
+      res.status(200).json(movie);
+  } else {
+      res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
+  }
+}));
 
 router.post('/', (req, res) => {
   let newMovie = req.body;
--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,8 +1,10 @@
 import userModel from '../api/users/userModel';
 import genreModel from '../api/genres/genreModel';
+import movieModel from '../api/movies/movieModel';
 
 import users from './users';
 import genres from "./genres";
+import movies from './movies.js';
 
 import dotenv from 'dotenv';
 
@@ -32,7 +34,20 @@ async function loadGenres() {
   }
 }
 
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
+}
+
 if (process.env.SEED_DB) {
   loadUsers();
   loadGenres();
+  loadMovies()
 }
